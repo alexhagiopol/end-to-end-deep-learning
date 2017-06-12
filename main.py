d75272a73fe3574ba4da6a505176662f1889bcd2
Alex Hagiopol <alexhagiopol@gmail.com>
2017-06-12 13:47:26 -0700

    visualization working for first 5 images

--- main.py
+++ main.py
@@ -25,9 +25,10 @@ def preprocessing(input_dir, output_dir):
     if not os.path.exists(image_output_dir):
         os.mkdir(image_output_dir)
     image_path_list = glob.glob(os.path.join(input_dir, '*.jpg'))
-    for image_path in image_path_list:
+    for image_path in image_path_list[0:5]:
         image = cv2.imread(image_path)
-        show_image((1, 1, 1), 'alex title', image, 5)
+        bgr_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # convert to BGR for opencv visualization
+        show_image((1, 1, 1), image_path, bgr_image, 3)  # location, path, image, width?
 
 
 if __name__ == "__main__":
