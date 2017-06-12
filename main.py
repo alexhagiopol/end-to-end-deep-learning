bf4a23295285c58e40eeefc25832b09e34ee969e
Alex Hagiopol <alexhagiopol@gmail.com>
2017-06-12 15:26:36 -0700

    basic preprocessing: grayscale + cropping

--- main.py
+++ main.py
@@ -25,11 +25,16 @@ def preprocessing(input_dir, output_dir):
     if not os.path.exists(image_output_dir):
         os.mkdir(image_output_dir)
     image_path_list = glob.glob(os.path.join(input_dir, '*.jpg'))
-    for image_path in image_path_list[0:5]:
+    for image_path in image_path_list:
+        image_name = image_path[len(input_dir) + 1:]
         image = cv2.imread(image_path)
         bgr_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # convert to BGR for opencv visualization
-        show_image((1, 1, 1), image_path, bgr_image, 3)  # location, path, image, width?
-
+        gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
+        cropped_image = gray_image[70:137, 0:]
+        cv2.imwrite(os.path.join(output_dir, image_name), cropped_image)
+        print('preprocessed ', image_name, ' to path ', output_dir)
+        # print(gray_image.shape)
+        # show_image((1, 1, 1), image_path, cropped_image, 3)  # location, path, image, width?
 
 if __name__ == "__main__":
     image_input_dir = 'data/images'
