66463727bb9e3a1faa1ed45816ca90ccd9561256
Alex Hagiopol <alexhagiopol@gmail.com>
2017-06-22 16:19:21 -0700

    WIP

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn.utils import shuffle
 if __name__ == "__main__":
     # load data from pickle file
     data_file = 'pickle_data.p'
-    utilities.preprocess('data', 'images', max_num_measurements=1000)
+    # utilities.preprocess('data', 'images', max_num_measurements=1000)
     with open(os.path.join('data', data_file), mode='rb') as f:
         pickle_data = pickle.load(f)
     X = pickle_data['features']
@@ -32,4 +32,4 @@ if __name__ == "__main__":
 
     model = architecture.test_model()
     model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
-    #model.save('alex_model.h5')
+    model.save('alex_model.h5')
