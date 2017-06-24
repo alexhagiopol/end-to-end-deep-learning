37296b0a1011b7e32894a003330fee11096a0546
Alex Hagiopol <alexhagiopol@gmail.com>
2017-06-23 18:33:38 -0700

    add even simpler architecture

--- architecture.py
+++ architecture.py
@@ -3,12 +3,20 @@ from keras.layers import Lambda, Flatten, Dense, Conv2D, ELU, Dropout
 from keras.utils import plot_model
 
 
-def test_model(input_shape=(67, 320)):
+def test_model_1(input_shape=(67, 320)):
     # create model
     model = Sequential()
     # normalize pixels to between -1 <= x <= 1
     model.add(Lambda(lambda x: (x / 255.0) * 2 - 1, input_shape=input_shape))
     model.add(Flatten())
+    model.add(Dense(1))
+    model.compile(loss="mse", optimizer="adam")
+    return model
+
+
+def test_model_2():
+    model = Sequential()
+    model.add(Flatten(input_shape=(67, 320)))
     model.add(Dense(1))
     model.compile(loss="mse", optimizer="adam")
     return model
