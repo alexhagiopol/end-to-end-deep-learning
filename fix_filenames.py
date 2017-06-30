6a9f475ea184fd85057baceb5f79cfa49c6ab689
Alex Hagiopol <alexhagiopol@gmail.com>
2017-06-30 13:51:51 -0700

    filename fixer finally handles NaN

--- fix_filenames.py
+++ fix_filenames.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import sys
+import numpy as np
+import math
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -9,17 +11,20 @@ if __name__ == "__main__":
         driving_log = pd.read_csv(csv_path, header=None)
         num_records = driving_log.shape[0]
         record_index = 0
+        print(driving_log.isnull().values)
         while record_index < num_records:
             center_file_path = driving_log.iloc[record_index, 0]
             left_file_path = driving_log.iloc[record_index, 1]
             right_file_path = driving_log.iloc[record_index, 2]
-            print("Fixing record row #", record_index, " of ", num_records, center_file_path, left_file_path,
-                  right_file_path)
-            driving_log.iloc[record_index, 0] = center_file_path[-34:]
-            driving_log.iloc[record_index, 1] = left_file_path[-32:]
-            driving_log.iloc[record_index, 2] = right_file_path[-33:]
-            record_index += 1
+            if str(center_file_path) == "nan" or str(left_file_path) == "nan" or str(right_file_path) == "nan":
+                driving_log.drop(driving_log.index[record_index], inplace=True)  # delete this row and don't advance the record index
+                num_records = driving_log.shape[0]
+            else:
+                print("Fixing record row #", record_index, " of ", num_records, center_file_path, left_file_path, right_file_path)
+                driving_log.iloc[record_index, 0] = center_file_path[-34:]
+                driving_log.iloc[record_index, 1] = left_file_path[-32:]
+                driving_log.iloc[record_index, 2] = right_file_path[-33:]
+                record_index += 1
         print("Overwriting CSV file: ", csv_path)
         driving_log.to_csv(csv_path, header=None, index=False)
         print("Done.")
-
