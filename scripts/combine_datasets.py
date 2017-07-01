ba4a8b6f25ccaebe3c93c04cc18ba6a2e114db80
Alex Hagiopol <alexhagiopol@gmail.com>
2017-07-01 14:40:26 -0700

    used script to create combined ian dataset

--- scripts/combine_datasets.py
+++ scripts/combine_datasets.py
@@ -2,8 +2,8 @@ import glob
 import shutil
 
 if __name__ == "__main__":
-    img_image_paths = glob.glob('driving_data/IMG/*')
-    img1_image_paths = glob.glob('driving_data/IMG1/*')
-    destination_path = 'driving_data/ian_images'
+    img_image_paths = glob.glob('data/IMG0/*')
+    img1_image_paths = glob.glob('data/IMG1/*')
+    destination_path = 'data/combined'
     for path in img_image_paths + img1_image_paths:
         shutil.move(path, destination_path)
